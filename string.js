071ffe36eb555e1f1f8ec10ba69a28380d619ffa
Hema Lohana <hemalohana8@gmail.com>
2024-02-10 22:53:09 +0530

    string file added

---
+++ string.js
@@ -0,0 +1,16 @@
+// let str = new String('John');
+// let str = 'John';
+
+// let str = 'Welcome to coding world!';
+
+// let substring1 = str.substring(11);
+
+// console.log(substring1); // coding world!
+
+// let substring1 = str.substring(11, 17);
+
+// console.log(substring1); // coding
+
+// let str1 = str.substr(11, 6);
+
+// console.log(str1); // coding
