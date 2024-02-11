0ff227f31548fb0cdc7e839b7be6f68ea4863d91
Hema Lohana <hemalohana8@gmail.com>
2024-02-11 22:15:10 +0530

    added new filr

---
+++ homework.js
@@ -0,0 +1,49 @@
+// #------------------------- Assisgnment 1 ---------------------#
+
+// 1. Given an array of numbers, return an array that contains squares of every elements.
+// input = [1, 2, 3, 4];
+// output = [1, 4, 9, 16]
+
+// 2. Find the maximum sum of subset of an array.
+// Note: A subset is an array formed by removing some or possibly none of the elements of array
+// and then forming a new array from the remaining elements.
+// input = [1, -3, 5, -9, 9, 7]
+// output = 22
+
+// 3. You are given an array of objects representing a group of students,
+// each with a name and an array of test scores. Your task is to use map, filter,
+// and reduce to calculate the average test score for each student, and then return
+// an array of objects containing only the students who have an average score above 90.
+// input =
+// [
+//   { name: "Alice", scores: [90, 85, 92] },
+//   { name: "Bob", scores: [75, 80, 85] },
+//   { name: "Charlie", scores: [90, 95, 85] },
+//   { name: "Jack", scores: [100, 100, 100] }
+// ];
+// output = 
+// [ 
+//     { name: 'Jack', average: 100 }
+// ]
+
+// 4. You are given an array of objects representing a collection of products,
+// each with a name, price, and category. Your task is to use map, filter, and
+// reduce to calculate the average price of products in each category, and then
+// return an array of objects containing only the categories that have an average
+// price above 50.
+// input =
+//  [
+//     { name: "Product 1", price: 20, category: "Electronics" },
+//     { name: "Product 2", price: 30, category: "Clothes" },
+//     { name: "Product 3", price: 40, category: "Electronics" },
+//     { name: "Product 4", price: 50, category: "Clothes" },
+//     { name: "Product 5", price: 60, category: "Clothes" },
+//     { name: "Product 6", price: 70, category: "Electronics" },
+//     { name: "Product 7", price: 80, category: "Clothes" },
+//     { name: "Product 8", price: 90, category: "Electronics" },
+//   ];
+// output =
+// [ 
+//     { category: 'Clothes', average: 55 },
+//     { category: 'Electronics', average: 55 }
+// ]
