e1075bef40ab8867a14060f91234318513d21f23
Hema0202 <hemalohana8@gmail.com>
2024-07-15 23:52:21 +0530

    f2 updated

--- f2/script.js
+++ f2/script.js
@@ -10,9 +10,9 @@
 // const interface = "Audio";
 // const private = 534;
 
-function logger() {
-  console.log("My name is Hema");
-}
+// function logger() {
+//   console.log("My name is Hema");
+// }
 
 //calling / running / invoking function
 // logger();
@@ -190,20 +190,20 @@ function logger() {
 // let tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 // console.log(bills, tips);
 
-const jonas = {
-  firstName: "Jonas",
-  lastName: "Schedtmann",
-  age: 2037 - 1991,
-  job: "teacher",
-  friends: ["Michael", "Peter", "Steven"],
-};
-console.log(jonas);
-console.log(jonas.lastName);
-console.log(jonas["lastName"]);
+// const jonas = {
+//   firstName: "Jonas",
+//   lastName: "Schedtmann",
+//   age: 2037 - 1991,
+//   job: "teacher",
+//   friends: ["Michael", "Peter", "Steven"],
+// };
+// console.log(jonas);
+// console.log(jonas.lastName);
+// console.log(jonas["lastName"]);
 
-const nameKey = "Name";
-console.log(jonas["first" + nameKey]);
-console.log(jonas["last" + nameKey]);
+// const nameKey = "Name";
+// console.log(jonas["first" + nameKey]);
+// console.log(jonas["last" + nameKey]);
 
 // const interestedIn = prompt(
 //   "what do u want to know about jonas? choose between firstName, lastName, age, job and friends"
@@ -222,6 +222,184 @@ console.log(jonas["last" + nameKey]);
 // console.log(jonas);
 
 //challenge
-console.log(
-  `${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`
-);
+// console.log(
+//   `${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`
+// );
+
+// const jonas = {
+//   firstName: "Jonas",
+//   lastName: "Schedtmann",
+//   birthyear: 1991,
+//   job: "teacher",
+//   friends: ["Michael", "Peter", "Steven"],
+//   hasDriversLicence: true,
+
+// calcAge: function (birthyear) {
+//   return 2037 - birthyear;
+// },
+
+// calcAge: function () {
+//   console.log(this);
+//   return 2037 - this.birthyear;
+// },
+
+//   calcAge: function () {
+//     this.age = 2037 - this.birthyear;
+//     return this.age;
+//   },
+// };
+
+// console.log(jonas.calcAge());
+// console.log(jonas.age);
+// console.log(jonas["calcAge"](1991));
+
+//challenge
+// console.log(
+//   `${jonas.firstName} is a ${jonas.age}-year old ${jonas.job}, and he has ${
+//     jonas.hasDriversLicence ? "a" : "no"
+//   } drivers's license`
+// );
+
+//challenge
+// const mark = {
+//   fullName: "Mark Miller",
+//   mass: 78,
+//   height:1.69,
+//   calcBMI: function(){
+//       let bmi = this.mass / (this.height * this.height);
+//       return bmi;
+//   }
+// }
+
+// const john = {
+//   fullName : "John Smith",
+//   mass: 52,
+//   height: 1,
+//   calcBMI: function(){
+//       let bmi = this.mass / (this.height * this.height);
+//       return bmi;
+//   }
+// }
+
+// if(mark.calcBMI() > john.calcBMI()){
+//   console.log(`Mark Miller BMI ${mark.calcBMI()} is higher than John Smith ${john.calcBMI()}`);
+// } else{
+//    console.log(`John Smith ${john.calcBMI()} is higher than Mark Miller BMI ${mark.calcBMI()}`);
+// }
+
+// console.log("Lifting weights repetition 1");
+// console.log("Lifting weights repetition 2");
+// console.log("Lifting weights repetition 3");
+// console.log("Lifting weights repetition 4");
+// console.log("Lifting weights repetition 5");
+// console.log("Lifting weights repetition 6");
+
+//for loop keeps running while condition is true
+// for (let rep = 1; rep <= 10; rep++) {
+//   console.log(`Lifting weights repetition ${rep}`);
+// }
+
+// const jonas = [
+//   "Jonas",
+//   "Schemedtmann",
+//   2037 - 1991,
+//   "teacher",
+//   ["Michael", "Peter", "Steven"],
+//   true,
+// ];
+// const types = [];
+
+// console.log(jonas[0])
+// console.log(jonas[1])
+// ...
+// console.log(jonas[4])
+
+// for (let i = 0; i < jonas.length; i++) {
+//   //Reading from jonas array
+//   console.log(jonas[i], typeof jonas[i]);
+
+//Filling types array
+//   types[i] = typeof jonas[i];
+//   types.push(typeof jonas[i]);
+// }
+
+// types[0] = "String";
+// console.log(types);
+
+// const years = [1991, 2007, 1969, 2020];
+// const ages = [];
+
+// for (let i = 0; i < years.length; i++) {
+//   ages.push(2037 - years[i]);
+// }
+// console.log(ages);
+
+//continue and break
+// console.log("---ONLY SRINGS---");
+// for (let i = 0; i < jonas.length; i++) {
+//   if (typeof jonas[i] !== "string") continue;
+//   console.log(jonas[i], typeof jonas[i]);
+// }
+
+// console.log("---BREAK WITH NUMBER---");
+// for (let i = 0; i < jonas.length; i++) {
+//   if (typeof jonas[i] === "number") break;
+//   console.log(jonas[i], typeof jonas[i]);
+// }
+
+// const jonas = [
+//   "Jonas",
+//   "Schemedtmann",
+//   2037 - 1991,
+//   "teacher",
+//   ["Michael", "Peter", "Steven"],
+//   true,
+// ];
+
+//4,3,...,0
+
+// for (let i = jonas.length - 1; i >= 0; i--) {
+//   console.log(i, jonas[i]);
+// }
+
+// for (let exercise = 1; exercise < 4; exercise++) {
+//   console.log(`------Starting exercise ${exercise}`);
+//   for (let rep = 1; rep < 6; rep++) {
+//     console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}`);
+//   }
+// }
+
+// for (let rep = 1; rep <= 10; rep++) {
+//   console.log(`Lifting weights repetition ${rep}`);
+// }
+
+// let rep = 1;
+// while (rep <= 10) {
+//   console.log(`WHILE: Lifting weights repetition ${rep}`);
+//   rep++;
+// }
+
+// let dice = Math.trunc(Math.random() * 6) + 1;
+// console.log(dice);
+
+// while (dice !== 6) {
+//   console.log(`You rolled a ${dice}`);
+//   dice = Math.trunc(Math.random() * 6) + 1;
+//   if (dice == 6) console.log("Loop is about to end..");
+// }
+
+//challenge
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+  const tip = calcTip(bills[i]);
+  tips.push(tip);
+  totals.push(tip + bills[i]);
+}
+console.log(bills, tips, totals);
---
+++ f2/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+= {
+//   firstName: "Jonas",
+//   lastName: "Schedtmann",
+//   birthyear: 1991,
+//   job: "teacher",
+//   friends: ["Michael", "Peter", "Steven"],
+//   hasDriversLicence: true,
